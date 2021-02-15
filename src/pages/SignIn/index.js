6d96c90da9dbd8bf8faf9d7ba0670007fa22d9db
cezaryi <cezariomourao@hotmail.com>
2021-02-15 13:07:49 -0300

    SignIn/SignUp

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
-// import { Container } from './styles';
+import logo from '../../assets/logo.svg';
 
 function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <>
+    <img src={logo} alt="Mobills"/>
+
+    <form>
+      <input type= "email" placeholder="Seu Email"/>
+      <input type= "password" placeholder="Senha"/>
+
+      <button type="submit">Acessar</button>
+      <Link to= "/register">Criar Conta</Link>
+      
+    </form>
+    </>
+  );
 }
 
 export default SignIn;
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
-// import { Container } from './styles';
+import logo from '../../assets/logo.svg';
 
 function SignUp() {
-  return <div />;
+  return (
+    <>
+    <img src={logo} alt="Mobills"/>
+
+    <form>
+      <input placeholder="Nome Completo"/>
+      <input type= "email" placeholder="Seu Email"/>
+      <input type= "password" placeholder="Senha"/>
+
+      <button type="submit">Criar Conta</button>
+      <Link to= "/">Já possuo Login</Link>
+      
+    </form>
+    </>
+  );
 }
 
 export default SignUp;
